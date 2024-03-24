8d9fd824ef6b713337c103e6219a8d9553b14a20
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2024-03-24 12:50:36 +0100

    making view for managing user labels

--- musicmix/forms.py
+++ musicmix/forms.py
@@ -4,3 +4,10 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label="username", max_length=20, required=True)
     password = forms.CharField(label="password", max_length=255, required=True)
+
+
+class LabelRegistrationForm(forms.Form):
+
+    def __init__(self):
+        super().__init__()
+
--- musicmix/urls.py
+++ musicmix/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .musicviews import mainviews
+from .musicviews import mainviews, userviews
 
 urlpatterns = [
     path("", mainviews.index, name="index"),
-    path("login", mainviews.login_user, name="login")
+    path("login", mainviews.login_user, name="login"),
+    path("labels", userviews.LabelRegistrationView.as_view())
 ]
