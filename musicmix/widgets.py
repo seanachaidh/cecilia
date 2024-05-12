172772d4ada6712e86af22121d2ac5a9e5f38670
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2024-05-12 09:08:43 +0200

    chips een beetje veranderen

--- musicmix/widgets.py
+++ musicmix/widgets.py
@@ -1,6 +1,10 @@
 from django.forms.widgets import Widget, RadioSelect
 
 class ChipWidget(RadioSelect):
+    class Media:
+        css = {
+            "all": ["/css/chips.css"]
+        }
     allow_multiple_selected = True
     option_template_name = 'musicmix/widgets/chip.html'
     template_name = 'musicmix/widgets/chip-container.html'
