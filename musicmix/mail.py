28a0d61876388b766b8590ecbdc023655fb66972
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2024-06-26 07:18:33 +0200

    Creating an email client

--- musicmix/mail.py
+++ musicmix/mail.py
@@ -18,8 +18,12 @@ class EmailSender:
     def __init__(self):
         self.hostname = settings.EMAIL_HOST
         self.client = smtplib.SMTP_SSL(self.hostname, 587)
+        # TODO moet ik ervoor zorgen dat de login aan blijft staan?
+        self._login_and_init()
     def _login_and_init(self):
         self.client.login(settings.EMAIL_USER, settings.EMAIL_PASSWORD)
         self.client.starttls()
         self.client.ehlo()
+    def send(self, email: Email):
+        self.client.send_message(email.message)
         
