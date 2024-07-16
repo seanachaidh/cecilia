cdb799d0917c119d07b88418e433b84c908d6745
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2024-07-16 07:03:51 +0200

    adding and removing users

--- musicmix/musicviews/adminviews.py
+++ musicmix/musicviews/adminviews.py
@@ -1,21 +1,29 @@
-
-from django.shortcuts import redirect, render
-from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect, render, reverse
 from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
+from django.views.decorators.http import require_POST
+
 from ..forms import UserCreationForm
 from ..models import *
 from random import randint
 
+# TODO maak hier een paged list view van
 def show_admin_panel(request):
     check = perform_auth_check(request)
-    if check != True:
+    if not check:
         return check
-    users = Profile.objects.all()
+    users = User.objects.all()
     admin_context = {
         "users": users
     }
     return render(request=request, template_name='musicmix/adminpanel.html', context=admin_context)
     
+@require_POST
+def remove_user(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.delete()
+    return redirect(reverse('admin'))
+
 
 def add_user(request):
     check = perform_auth_check(request)
@@ -37,12 +45,12 @@ def add_user(request):
                 is_superuser=is_admin
             )
             new_user.save()
-            #Hier het wachtwoord resettten
-            
+            #Hier het wachtwoord resetten
+            return redirect(reverse('admin'))
             
     else:
         form = UserCreationForm()
-    return render(request, 'musicmix/usercreation.html', {"form": form})
+    return render(request, 'musicmix/basic_form.html', {"form": form})
     
 
 def perform_auth_check(request):
@@ -51,11 +59,11 @@ def perform_auth_check(request):
     if not request.user.is_superuser:
         raise PermissionDenied()
     return True
-
-def random_string(len: int) -> str:
-    letters = "1234567890ABCDEFGHIJKLMNOP"
+#TODO verander dit door een functie van django zelve
+def random_string(length: int) -> str:
+    letters = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     result = ''
-    for i in range(len):
+    for i in range(length):
         nummer = randint(0, len(letters) - 1)
         gekozen = letters[nummer]
         result = result + gekozen
--- musicmix/urls.py
+++ musicmix/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", userviews.MyPiecesView.as_view(), name="index"),
     path("admin", adminviews.show_admin_panel, name="admin"),
     path("admin/createuser", adminviews.add_user, name="creatuser"),
+    path("admin/deleteuser/<int:user_id>", adminviews.remove_user, name="remove_user"),
     path("login", mainviews.login_user, name="login"),
     path("logout", mainviews.logout_user, name="logout"),
     path("labels", userviews.LabelRegistrationView.as_view()),
