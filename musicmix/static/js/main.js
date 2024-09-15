03b01b6cf52adfc9b58ba9b2082b71914ecf9af2
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2024-09-15 10:39:01 +0200

    vorige knop implementeren

--- musicmix/static/js/main.js
+++ musicmix/static/js/main.js
@@ -1,26 +1,6 @@
-/**
- * Initialiseert alles voor een dialoog
- * @param {string} dialogParam parameter van die de id van de dialoog moet voorstellen
- * @param {string} openBtn De button die de dialoog opent
- * @param {string} closeBtn De button die de dialoog stluit
- * @returns {void}
- */
-function initializeDialog(dialogParam, closeBtn, openBtn) {
-    let dialogElem = document.getElementById(dialogParam);
-    let closeElem = document.getElementById(closeBtn);
-    let openElem = document.getElementById(openBtn);
+console.log('script geladen');
 
-
-    openElem.addEventListener("click", openClick);
-    closeElem.addEventListener("click", closeClick);
-
+function goBack() {
+    console.log('Terugknop ingedrukt');
+    history.back();
 }
-
-function openClick() {
-    
-}
-
-function closeClick() {
-    
-}
-
