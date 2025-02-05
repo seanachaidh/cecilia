e7d3397de7e68277f1c87f829255b32091dffa1e
seanachaidh <pvankeymeulen@seanachaidh.be>
2025-02-05 23:11:16 +0100

    label registratie maken

--- musicmix/musicviews/userviews.py
+++ musicmix/musicviews/userviews.py
@@ -1,8 +1,6 @@
-from logging import info
 
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView
@@ -11,23 +9,6 @@ from ..dao.labelrepo import fetch_labels_for_profile, get_labels_from_ids
 from ..dao.profilerepo import find_or_create_profile
 from ..forms import LabelRegistrationForm
 from ..models import MusicPiece, Profile
-
-
-# This function is way too long and can probably be written shorter
-# @login_required(login_url='login')
-def populate_form(user: User):
-    data = dict()
-    profile = find_or_create_profile(user)
-    labels = fetch_labels_for_profile(profile)
-    for label in labels:
-        key = str(label.label_type.lower())
-        value = str(label.pk)
-
-        label_value_list = data.get(key, [])
-        label_value_list.append(value)
-        data[key] = label_value_list
-
-    return data
 
 class OverviewView(LoginRequiredMixin, ListView):
     model = MusicPiece
@@ -58,12 +39,32 @@ class MyPiecesView(LoginRequiredMixin, ListView):
         labels = profile.labels.all()
         return MusicPiece.objects.filter(active=True, labels__in=labels)
 
+
+#TODO zet alles dat te maken heeft met labels in een appart bestand
+class LabelData:
+    
+    def __init__(self, type, naam: str, geselecteerd: bool):
+        self._type = type
+        self._naam = naam
+        self._geselecteerd = geselecteerd
+
+    @property
+    def type(self):
+        return self._type
+    @property
+    def naam(self) -> str:
+        return self._naam
+    @property
+    def geselecteerd(self) -> bool:
+        return self._geselecteerd
+
+    
+
 class LabelRegistrationView(LoginRequiredMixin, View):
     login_url = 'login'
 
     def get(self, request):
-        d = populate_form(request.user)
-        form = LabelRegistrationForm(data=d)
+        form = LabelRegistrationForm()
         return render(request=request, template_name='musicmix/labelregistration.html', context={'form': form})
 
     def post(self, request):
@@ -76,7 +77,6 @@ class LabelRegistrationView(LoginRequiredMixin, View):
             stem_objects = get_labels_from_ids(stem)
             instrument_objects = get_labels_from_ids(instruments)
 
-            info("saving labels")
             profile = find_or_create_profile(request.user)
             profile.labels.set(stem_objects + instrument_objects)
             profile.save()
